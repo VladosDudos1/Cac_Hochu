b680e542b210671ee62235bcb01689ddd8a2c686
VladosDudos1 <55552724+VladosDudos1@users.noreply.github.com>
2020-12-24 11:24:33 +0300

    init

---
+++ AccountingApp/Model/GoodsMovement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingApp.Model
+{
+    public class GoodsMovement
+    {
+        public int Id { get; set; }
+        public string Product { get; set; }
+        public int Amount { get; set; }
+        public decimal Price { get; set; }
+        public decimal Sum { get; set; }
+        public DateTime Date { get; set; }
+
+        public GoodsMovement(int id, string product, int amount, decimal price, DateTime date)
+        {
+            this.Id = id;
+            this.Product = product;
+            this.Amount = amount;
+            this.Price = price;
+            this.Sum = price * amount;
+            this.Date = date;
+        }
+    }
+}
---
+++ AccountingApp/Pages/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using AccountingApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AccountingApp.Pages
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+
+        List<GoodsMovement> sales = new List<GoodsMovement>()
+        {
+            new GoodsMovement(1, "Product1", 2, 30, new DateTime(2020,1,3)),
+            new GoodsMovement(2, "Product2", 10, 100, new DateTime(2020,1,9)),
+            new GoodsMovement(3, "Product3", 5, 20, new DateTime(2020,3,1)),
+            new GoodsMovement(4, "Product4", 6, 45, new DateTime(2020,2,1)),
+            new GoodsMovement(5, "Product5", 4, 23, new DateTime(2020,3,22)),
+            new GoodsMovement(6, "Product6", 2, 3125, new DateTime(2020,1,11)),
+        };
+
+        List<GoodsMovement> arrivals = new List<GoodsMovement>()
+        {
+            new GoodsMovement(1, "Product1", 2, 30, new DateTime(2020,11,3)),
+            new GoodsMovement(2, "Product2", 10, 100, new DateTime(2020,12,9)),
+            new GoodsMovement(3, "Product3", 5, 20, new DateTime(2020,4,5)),
+            new GoodsMovement(4, "Product4", 6, 45, new DateTime(2020,4,3)),
+            new GoodsMovement(5, "Product5", 4, 23, new DateTime(2020,6,22)),
+            new GoodsMovement(6, "Product6", 2, 3125, new DateTime(2020,6,11)),
+        };
+        public MainPage()
+        {
+            InitializeComponent();
+            DGSale.ItemsSource = sales;
+            DGArrival.ItemsSource = arrivals;
+        }
+    }
+}
